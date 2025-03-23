a7b53afa6f3e3e436697785e6d48a37f342e90f8
yawwnann <c0478592@gmail.com>
2025-03-23 21:15:30 +0800

    perbaikan

--- src/pages/CityDetails.tsx
+++ src/pages/CityDetails.tsx
@@ -1,67 +1,86 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { City } from '../types/type';
-import OfficeCard from '../components/OfficeCard';
-import NavbarWrapper from '../wrappers/NavbarWrapper';
-import { config } from '../config';
-import apiClient from '../services/apiService';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { City } from "../types/type";
+import OfficeCard from "../components/OfficeCard";
+import NavbarWrapper from "../wrappers/NavbarWrapper";
+import { config } from "../config";
+import apiClient from "../services/apiService";
 
 export default function CityDetails() {
-    const { slug } = useParams<{ slug: string }>();
-    const [city, setCity] = useState<City | null>(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<string | null>(null);
-    const baseURL = `${config.baseURL}/storage`;
+  const { slug } = useParams<{ slug: string }>();
+  const [city, setCity] = useState<City | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const baseURL = `${config.baseURL}/storage`;
 
-    useEffect(() => {
-        apiClient
-            .get(`/api/city/${slug}`)
-            .then((response) => {
-                setCity(response.data.data);
-                setLoading(false);
-            })
-            .catch((error) => {
-                setError(error);
-                setLoading(false);
-            });
-    }, [slug]);
+  useEffect(() => {
+    apiClient
+      .get(`/api/city/${slug}`)
+      .then((response) => {
+        setCity(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [slug]);
 
-    if (loading) {
-        return <p>Loading...</p>;
-    }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-    if (error) {
-        return <p>Error Loading : {error}</p>;
-    }
+  if (error) {
+    return <p>Error Loading : {error}</p>;
+  }
 
-    if (!city) {
-        return <p>City tidak ditemukan</p>;
-    }
+  if (!city) {
+    return <p>Data Kota tidak ditemukan</p>;
+  }
 
-    return (
-        <>
-            <NavbarWrapper />
-            <header className="flex flex-col w-full">
-                <section id="Hero-Banner" className="relative flex h-[434px]">
-                    <div id="Hero-Text" className="relative flex flex-col w-full max-w-[650px] h-fit rounded-[30px] border border-[#E0DEF7] p-10 gap-[30px] bg-white mt-[70px] ml-[calc((100%-1130px)/2)] z-10">
-                        <h1 className="font-extrabold text-[50px] leading-[60px]">
-                            Great Office in <br /> <span className="text-[#0D903A]">{city.name}</span>
-                        </h1>
-                        <p className="text-lg leading-8 text-[#000929]">Kantor yang tepat dapat memberikan impact pekerjaan menjadi lebih baik dan sehat dalam tumbuhkan karir.</p>
-                    </div>
-                    <div id="Hero-Image" className="absolute right-0 w-[calc(100%-((100%-1130px)/2)-305px)] h-[434px] rounded-bl-[40px] overflow-hidden">
-                        <img src={`${baseURL}/${city.photo}`} className="w-full h-full object-cover" alt="hero background" />
-                    </div>
-                </section>
-            </header>
-            <section id="Fresh-Space" className="flex flex-col gap-[30px] w-full max-w-[1130px] mx-auto mt-[70px] mb-[120px]">
-                <h2 className="font-bold text-[32px] leading-[48px] text-nowrap">Browse Offices</h2>
-                <div className="grid grid-cols-3 gap-[30px]">
-                    {city.officeSpaces.map((office) => (
-                        <OfficeCard key={office.id} office={office}></OfficeCard>
-                    ))}
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <NavbarWrapper />
+      <header className="flex flex-col w-full">
+        <section id="Hero-Banner" className="relative flex h-[434px]">
+          <div
+            id="Hero-Text"
+            className="relative flex flex-col w-full max-w-[650px] h-fit rounded-[30px] border border-[#E0DEF7] p-10 gap-[30px] bg-white mt-[70px] ml-[calc((100%-1130px)/2)] z-10"
+          >
+            <h1 className="font-extrabold text-[50px] leading-[60px]">
+              Great Office in <br />{" "}
+              <span className="text-[#0D903A]">{city.name}</span>
+            </h1>
+            <p className="text-lg leading-8 text-[#000929]">
+              Kantor yang tepat dapat memberikan impact pekerjaan menjadi lebih
+              baik dan sehat dalam tumbuhkan karir.
+            </p>
+          </div>
+          <div
+            id="Hero-Image"
+            className="absolute right-0 w-[calc(100%-((100%-1130px)/2)-305px)] h-[434px] rounded-bl-[40px] overflow-hidden"
+          >
+            <img
+              src={`${baseURL}/${city.photo}`}
+              className="w-full h-full object-cover"
+              alt="hero background"
+            />
+          </div>
+        </section>
+      </header>
+      <section
+        id="Fresh-Space"
+        className="flex flex-col gap-[30px] w-full max-w-[1130px] mx-auto mt-[70px] mb-[120px]"
+      >
+        <h2 className="font-bold text-[32px] leading-[48px] text-nowrap">
+          Browse Offices
+        </h2>
+        <div className="grid grid-cols-3 gap-[30px]">
+          {city.officeSpaces.map((office) => (
+            <OfficeCard key={office.id} office={office}></OfficeCard>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
